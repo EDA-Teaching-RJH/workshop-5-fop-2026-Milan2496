c4b57435fc73a2d31c8617a0cf62b085c9df2ffd
Milan2496 <mg101@kent.ac.uk>
2026-02-03 12:52:39 +0000

    coffee reliable with notes

--- coffee.py
+++ coffee.py
@@ -1,11 +1,20 @@
+# functional requirement
+# code must acceot only 50 20 10 5 as inputs
+# code must output the correct balance owed 
+
+# non functional requirement
+# if a string is inputted then it must ask the user to try again
+
+
+
 coinremain = 75
 coincount = 0
 
 while coincount < 75:
 
-    coin = int(input("A coffe costs 75p. Accepted coins = 50p, 20p, 10p, 5p. Please insert coin "))
-   
+    coin = (input("A coffe costs 75p. Accepted coins = 50p, 20p, 10p, 5p. Please insert coin "))
     if coin == 50 or coin == 20 or coin == 10 or coin == 5:
+        coin = int(coin)
         coincount = coincount + coin
         coinremain = coinremain - coin
         print(f"Balance inserted: {coincount}p")
@@ -16,7 +25,7 @@ while coincount < 75:
             owe = coincount - 75
             print(f"Balance owed: {owe}p")
     else:
-        print("This coin is not accepted")
+        print("Invalid input. This coin is not accepted")
 
 
 
