8a0a000ee42dbb088a54626763af9a0290a88738
Milan2496 <mg101@kent.ac.uk>
2026-02-03 12:34:07 +0000

    coffee

--- camel.py
+++ camel.py
@@ -1,1 +1,4 @@
-# Enter your code here
+camel = str(input("What is the name of your variable in camel case "))
+camel = camel.lower()
+camel = camel.replace(" ", "_")
+print("You're variable's name in snake case is " + camel)
--- coffee.py
+++ coffee.py
@@ -1,1 +1,22 @@
-#Your code goes here
+coinremain = 75
+coincount = 0
+
+while coincount < 75:
+
+    coin = int(input("A coffe costs 75p. Accepted coins = 50p, 20p, 10p, 5p. Please insert coin "))
+   
+    if coin == 50 or coin == 20 or coin == 10 or coin == 5:
+        coincount = coincount + coin
+        coinremain = coinremain - coin
+        print(f"Balance inserted: {coincount}p")
+
+        if coincount < 75:
+            print(f"Balance left to pay: {coinremain}p")
+        else:
+            owe = coincount - 75
+            print(f"Balance owed: {owe}p")
+    else:
+        print("This coin is not accepted")
+
+
+
